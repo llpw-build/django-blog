10b6ad508059682b99062b1030d1698e8e23bff2
llpw-build <llpw@hotmail.co.uk>
2026-05-03 20:03:35 +0100

    Deployment settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$5q-^u6zok_2(9qa&lc58nhi*uc(ha%k6776pe%xn5ga904bv'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
