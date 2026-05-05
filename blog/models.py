bd67a7f4bed55916ed3d7d2b0702292630997f8f
llpw-build <llpw@hotmail.co.uk>
2026-05-05 20:15:56 +0100

    Resolve bug

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
-class comment(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(
     Post,
     on_delete=models.CASCADE,
