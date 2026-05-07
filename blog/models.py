a8dc8b8ff5fc81ecc539caeafc333bafb2413fd4
llpw-build <llpw@hotmail.co.uk>
2026-05-07 19:11:14 +0100

    Add html view

--- blog/models.py
+++ blog/models.py
@@ -18,7 +18,6 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
-
     class Meta:
         ordering = ["-created_on"]
 
@@ -42,6 +41,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
     
 
     class Meta:
