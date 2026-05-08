fe3c91e5e8e11683aa21959ed72ee0664070fee2
llpw-build <llpw@hotmail.co.uk>
2026-05-08 19:57:58 +0100

    add about app

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
+
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
